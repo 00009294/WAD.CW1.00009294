f74192feea2e1e8a5b04c34c4cb560cd719190b6
Javlonbek fayzullaev <fayzullaev.javlonbek.d@gmail.com>
2023-03-29 17:33:57 +0500

    Done with controllers

---
+++ SampleDemoWebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,110 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SampleDemoWebAPI.Dto;
+using SampleDemoWebAPI.Interfaces;
+using SampleDemoWebAPI.Models;
+
+namespace SampleDemoWebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IStudentRepository _studentRepository;
+        private readonly IMapper _mapper;
+
+        public StudentsController(IStudentRepository studentRepository, IMapper mapper)
+        {
+            _studentRepository = studentRepository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult GetAllStudents()
+        {
+            var students = _mapper.Map<List<StudentDto>>(_studentRepository.GetAllStudents().ToList());
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(students);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult GetStudentById(int id)
+        {
+            var student = _mapper.Map<StudentDto>(_studentRepository.GetStudentById(id));
+            if (ModelState.IsValid)
+            {
+                return Ok(student);
+            }
+            ModelState.AddModelError("", "Not found");
+            return BadRequest(ModelState);
+        }
+
+        [HttpGet("{name}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult GetStudentByName(string name)
+        {
+            var student = _mapper.Map<StudentDto>(_studentRepository.GetStudentByName(name));
+            if (ModelState.IsValid)
+            {
+                return Ok(student);
+            }
+            //ModelState.AddModelError("", "Not found");
+            //return BadRequest(ModelState);
+            return NotFound();
+        }
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateStudent([FromBody] StudentDto studentDto)
+        {
+            if (studentDto == null) return BadRequest(ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var createdStudent = _mapper.Map<Student>(studentDto);
+            if (!_studentRepository.CreateStudent(createdStudent))
+            {
+                ModelState.AddModelError("", "Smth went wrong while creating");
+                return BadRequest(ModelState);
+            }
+            return Ok("Successfully created");
+        }
+        [HttpPut("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult UpdateStudent(int id, [FromBody] StudentDto studentDto)
+        {
+            if (studentDto == null) return BadRequest(ModelState);
+            var student = _studentRepository.GetStudentById(id);
+            if (student == null) return NotFound();
+            var updatedStudent = _mapper.Map<Student>(studentDto);
+            if (_studentRepository.UpdateStudent(updatedStudent))
+            {
+                return Ok("Successfully updated");
+            }
+            return BadRequest(ModelState);
+        }
+        [HttpDelete("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult DeleteStudent(int id)
+        {
+            var student = _studentRepository.GetStudentById(id);
+            if (student == null) return NotFound();
+
+            if (_studentRepository.DeleteStudent(student))
+            {
+                return Ok("Successfully deleted");
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ SampleDemoWebAPI/Controllers/TeachersController.cs
@@ -0,0 +1,97 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SampleDemoWebAPI.DbContexts;
+using SampleDemoWebAPI.Dto;
+using SampleDemoWebAPI.Interfaces;
+using SampleDemoWebAPI.Models;
+using SampleDemoWebAPI.Repositories;
+
+namespace SampleDemoWebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TeachersController:ControllerBase
+    {
+        private readonly ITeacherRepository _teacherRepository;
+        private readonly IMapper _mapper;
+
+        public TeachersController(ITeacherRepository teacherRepository ,IMapper mapper)
+        {
+            _teacherRepository = teacherRepository;
+            _mapper = mapper;
+        }
+        public IActionResult GetAllTeachers()
+        {
+            var teachers = _mapper.Map<List<TeacherDto>>(_teacherRepository.GetAllTeachers().ToList());
+            if(ModelState.IsValid)
+            {
+                return Ok(teachers);
+            }
+            return BadRequest(ModelState);
+        }
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult GetTeacherById(int id) {
+            var teacher = _teacherRepository.GetTeacherById(id);
+            if(ModelState.IsValid) return Ok(teacher);
+            return BadRequest(ModelState);
+        }
+        [HttpGet("{name}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult GetTeacherByName(string name)
+        {
+            var teacher = _teacherRepository.GetTeacherByName(name);
+            if (ModelState.IsValid) return Ok(teacher);
+            return BadRequest(ModelState);
+        }
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateTeacher([FromBody] TeacherDto teacherDto)
+        {
+            if (teacherDto == null) return BadRequest(ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var createdTeacher = _mapper.Map<Teacher>(teacherDto);
+            if (!_teacherRepository.CreateTeacher(createdTeacher))
+            {
+                ModelState.AddModelError("", "Smth went wrong while creating");
+                return BadRequest(ModelState);
+            }
+            return Ok("Successfully created");
+        }
+        [HttpPut("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult UpdateTeacher(int id, [FromBody] TeacherDto teacherDto)
+        {
+            if (teacherDto == null) return BadRequest(ModelState);
+            var teacher = _teacherRepository.GetTeacherById(id);
+            if (teacher == null) return NotFound();
+            var updatedTeacher= _mapper.Map<Teacher>(teacherDto);
+            if (_teacherRepository.UpdateTeacher(updatedTeacher))
+            {
+                return Ok("Successfully updated");
+            }
+            return BadRequest(ModelState);
+        }
+        [HttpDelete("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult DeleteTeacher(int id)
+        {
+            var teacher = _teacherRepository.GetTeacherById(id);
+            if (teacher == null) return NotFound();
+
+            if (_teacherRepository.DeleteTeacher(teacher))
+            {
+                return Ok("Successfully deleted");
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
